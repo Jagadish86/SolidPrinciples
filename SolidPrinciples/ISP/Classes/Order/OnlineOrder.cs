a2b70d61fc9430955e3f66ed7ebfe9e5364d74a1
jagapc <jagapc@DESKTOP-8HI6OIM>
2022-01-16 13:47:00 +0530

    Covered OCP and ISP

---
+++ SolidPrinciples/ISP/Classes/Order/OnlineOrder.cs
@@ -0,0 +1,20 @@
+﻿using SolidPrinciples.ISP.Interfaces.Order;
+using System;
+
+namespace SolidPrinciples.ISP.Classes.Order
+{
+    public class OnlineOrder : IOrder, IOnlineOrder
+    {
+        public void AddToCart()
+        {
+            //Do Add to Cart
+            Console.WriteLine("Do Add To Cart");
+        }
+
+        public void CCProcess()
+        {
+            // Process through credit card
+            Console.WriteLine("Process through credit card");
+        }
+    }
+}
---
+++ SolidPrinciples/ISP/Classes/Shape.cs
@@ -0,0 +1,7 @@
+﻿namespace SolidPrinciples.ISP.Classes
+{
+    public abstract class Shape
+    {
+        public abstract string GetShape();
+    }
+}
